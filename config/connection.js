93257d26ca20a06ca43b25cfedc1f88fbe4f5b45
Jacob Erling <jacobs_macbook@Jacobs-MacBook-Pro.local>
2017-07-20 00:14:44 -0400

    connection

--- config/connection.js
+++ config/connection.js
@@ -13,20 +13,5 @@ if (process.env.JAWSDB_URL) {
 
 };
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 module.exports = connection;
